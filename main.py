2a45fc66dec8c0d07758cb027c4e32f6619d1e36
ozcotech <ozkancomert@hotmail.com>
2025-05-08 23:52:19 +0300

    Register DisputeTypeScreen in ScreenManager and rename selected_dispute to English

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, SlideTransition
 from screens.start_screen import StartScreen  # First screen
-
+from screens.dispute_type_screen import DisputeTypeScreen  # Second screen
 
 class MedPayApp(App):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.selected_dispute = {}
+
     def build(self):
         sm = ScreenManager(transition=SlideTransition())
         sm.add_widget(StartScreen(name="start"))  # First screen
+        sm.add_widget(DisputeTypeScreen(name="dispute_type"))  # Second screen
         return sm
 
 if __name__ == "__main__":
