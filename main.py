de05f86229049b0ca244421afb1b8a9fa23c8d49
ozcotech <ozkancomert@hotmail.com>
2025-05-09 01:51:59 +0300

    Update main.py: integrate new screens and routing logic

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, SlideTransition
 from screens.start_screen import StartScreen  # First screen
 from screens.dispute_type_screen import DisputeTypeScreen  # Second screen
+from screens.dispute_category_screen import DisputeCategoryScreen
+from screens.dispute_detail_screen import DisputeDetailScreen
+from screens.non_agreement_detail_screen import NonAgreementDetailScreen
+from screens.result_screen import ResultScreen
 
 class MedPayApp(App):
     def __init__(self, **kwargs):
@@ -12,6 +16,10 @@ class MedPayApp(App):
         sm = ScreenManager(transition=SlideTransition())
         sm.add_widget(StartScreen(name="start"))  # First screen
         sm.add_widget(DisputeTypeScreen(name="dispute_type"))  # Second screen
+        sm.add_widget(DisputeCategoryScreen(name="dispute_category"))  # Third screen
+        sm.add_widget(DisputeDetailScreen(name="dispute_detail"))  # Fourth screen
+        sm.add_widget(NonAgreementDetailScreen(name="non_agreement_detail"))
+        sm.add_widget(ResultScreen(name="result"))
         return sm
 
 if __name__ == "__main__":
